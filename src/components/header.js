701d5c3dbf6099ad86664ae3867bcc8e67ad6f89
RJ Salvador <rjsalvadorr@gmail.com>
2022-01-07 16:15:36 -0500

    Starting to restyle header

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,15 @@ import NavigationToolbar from "./navigation-toolbar"
 
 const Header = ({ isHome, isPost }) => (
   <div className="header">
-    <div className="header-nav-background"></div>
     <div className="header-container">
       <div className="header-content">
         <div className="header-icon-wrapper">
           <img className="header-icon" src="/full-white.svg" alt="header icon" />
         </div>
+        <div className="header-toolbar-wrapper">
+          <NavigationToolbar />
+        </div>
       </div>
-      <NavigationToolbar />
     </div>
   </div>
 )
