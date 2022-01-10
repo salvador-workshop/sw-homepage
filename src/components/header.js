42acc09f68759ac06fa73c035e60b2ec1633bf1e
RJ Salvador <rjsalvadorr@gmail.com>
2022-01-10 14:18:05 -0500

    extending switchable UI styling to header/footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,20 @@
 import * as React from "react"
 
 import NavigationToolbar from "./navigation-toolbar"
+import SwitchableImage from "./switchable-image"
 
 const Header = ({ isHome, isPost, isLightUi, onLightUiChanged }) => (
   <div className="header">
     <div className="header-container">
       <div className="header-content">
         <div className="header-icon-wrapper">
-          <img className="header-icon" src="/full-white.svg" alt="header icon" />
+          <SwitchableImage
+            className="header-icon"
+            darkImgPath="/full-white.svg"
+            lightImgPath="/full-black.svg"
+            isLightUi={isLightUi}
+            alt="header icon"
+          />
         </div>
         <div className="header-flex-spacer" />
         <div className="header-toolbar-wrapper">
